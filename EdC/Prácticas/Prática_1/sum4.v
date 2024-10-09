451fbd1e9cfbf809545b200a34af528dd41c2c5c
Joancgm05 <alu0101656571@ull.edu.es>
2024-10-09 17:34:30 +0100

    mc

--- EdC/Prácticas/Prática_1/sum4.v
+++ EdC/Prácticas/Prática_1/sum4.v
@@ -6,5 +6,5 @@ module sum4(output wire[3:0] S, output wire c_out, input wire[3:0] A, input wire
   fa fa1 (S[1], c2, A[1], B[1], c1);
   fa fa2 (S[2], c3, A[2], B[2], c2);
   fa fa3 (S[3], c_out, A[3], B[3], c3);
-
+  
 endmodule
