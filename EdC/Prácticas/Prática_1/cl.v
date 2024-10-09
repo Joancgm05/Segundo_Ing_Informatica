bcb8621a4a89faa81ca75a09283353732928bef0
Joancgm05 <alu0101656571@ull.edu.es>
2024-10-09 17:14:38 +0100

    mc

--- EdC/Prácticas/Prática_1/cl.v
+++ EdC/Prácticas/Prática_1/cl.v
@@ -6,7 +6,7 @@ module cl(output wire out, input wire a, b, input wire [1:0] S);
   or  or1(salida_or, a, b);
   xor xor1(sx, a, b);
   not not1(salida_not, a);
-  mux4_1 mux4_1(Out, salida_and, salida_or, sx, salida_not, S);
+  mux4_1 mux4_1_1(Out, salida_and, salida_or, sx, salida_not, S);
 
 
 endmodule
