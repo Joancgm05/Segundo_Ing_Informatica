9e53985022ede587d264b3401855b149492505ab
Joancgm05 <alu0101656571@ull.edu.es>
2024-10-10 08:28:57 +0100

    mc

--- EdC/Prácticas/Prática_1/sum4_v2.v
+++ EdC/Prácticas/Prática_1/sum4_v2.v
@@ -1,6 +1,5 @@
 module sum4_v2(output wire[3:0] S, output wire c_out, input wire[3:0] A, input wire[3:0] B, input wire c_in);
 
-  wire c1, c2, c3;
-  assign {}
+  assign {c_out, S} = A + B + c_in;
 
 endmodule
