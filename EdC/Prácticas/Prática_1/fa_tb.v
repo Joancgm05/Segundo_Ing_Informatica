7ff0e9a3a97ce53addc93077e1cff43c81370d05
Joancgm05 <alu0101656571@ull.edu.es>
2024-10-13 19:05:41 +0100

    test bench fulla adder con operador de concatenación

--- EdC/Prácticas/Prática_1/fa_tb.v
+++ EdC/Prácticas/Prática_1/fa_tb.v
@@ -1,25 +1,20 @@
-// Testbench para modulo semisumador visto antes
+// Testbench para modulo full adder con operador de concatenación.
 
 `timescale 1 ns / 10 ps //Directiva que fija la unidad de tiempo de simulaci�n y el paso de simulaci�n
 
-module fa_vr_tb;
+module fa_tb;
 //declaracion de se�ales
 
-reg test_a, test_b, test_c_in; //las se�ales de entrada al semisumador. Se han declarado reg porque queremos inicializarlas
-wire test_sum, test_carry; //se�ales de salida, se declaran como wire porque sus valores se fijan por el semisumador
+reg test_a, test_b, test_c_in; //e han declarado reg porque queremos inicializarlas
+wire test_sum, test_carry; //se�ales de salida, se declaran como wire porque sus valores se fijan por el full adder
 
 //instancia del modulo a testear, con notaci�n de posiciones de argumentos 
-fa_vr fa_prueba (test_sum, test_carry, test_a, test_b, test_c_in);
-
-//Lo siguiente comentado es una notaci�n alternativa para instanciar el m�dulo, los par�metros se denotan con un punto seguido del
-//nombre del par�metro en la definici�n original del m�dulo y entre par�ntesis a qu� se conecta en el modulo actual
-//no importa el orden de los par�metros definidos as� -?
-//ha_v1 ha1(.a(test_a), .b(test_b), .sum(test_sum), .carry(test_carry)); 
+fa fa_prueba (test_sum, test_carry, test_a, test_b, test_c_in);
 
 initial
 begin
   $monitor("tiempo=%0d a=%b b=%b c_in=%b suma=%b acarreo=%b", $time, test_a, test_b, test_c_in, test_sum, test_carry);
-  $dumpfile("fa_vr_tb.vcd");
+  $dumpfile("fa_tb.vcd");
   $dumpvars;
 
   //vector de test 0
