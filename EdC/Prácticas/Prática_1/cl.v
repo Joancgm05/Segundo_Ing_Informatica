b073be1f2e8bf143de5e9ba863ee8e86af9dd660
Joancgm05 <alu0101656571@ull.edu.es>
2024-10-13 21:06:21 +0100

    mc cl.v

--- EdC/Prácticas/Prática_1/cl.v
+++ EdC/Prácticas/Prática_1/cl.v
@@ -1,18 +1,14 @@
-// Celda lógica
+// módulo de una Celda lógica
 
 module cl(output wire out, input wire a, b, input wire [1:0] S);
 
-  // Conexiones internas
-  wire salida_and, salida_or, salida_xor, salida_not;
+  wire salida_and, salida_or, salida_xor, salida_not;  // declaro los cables internos
 
-  // Instanciación de las puertas lógicas
-  and and1(salida_and, a, b);
-  or  or1(salida_or, a, b);
-  xor xor1(salida_xor, a, b);
-  not not1(salida_not, a);
+  and and1(salida_and, a, b); // declaramos puerta and.
+  or  or1(salida_or, a, b); // declaramos puerta or.
+  xor xor1(salida_xor, a, b); // declaramos puerta xor.
+  not not1(salida_not, a); // declaramkos puerta not.
 
-  // Instanciación del multiplexor 4 a 1
-  mux4_1 mux4_1_1(out, salida_and, salida_or, salida_xor, salida_not, S);
-
+  mux4_1 mux4_1_1(out, salida_and, salida_or, salida_xor, salida_not, S); // declaramos el multiplexor
 
 endmodule
