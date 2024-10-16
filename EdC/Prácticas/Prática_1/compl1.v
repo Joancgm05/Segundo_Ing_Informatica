a008f5e1bec85b9c9786d2641fee425824f06590
Joancgm05 <alu0101656571@ull.edu.es>
2024-10-16 15:28:49 +0100

    mc

--- EdC/Prácticas/Prática_1/compl1.v
+++ EdC/Prácticas/Prática_1/compl1.v
@@ -2,6 +2,16 @@
 
 module compl1(output wire [3:0] Out, input wire [3:0] Inp, input wire cpl);
 
+  always @(Inp, cpl)
+  
+    begin
+      case (cpl)
+        1:
+          Out = ~Inv(Inp);
+        0:
+          Out = Inp;
+      endcase
 
+    end
 
 endmodule
