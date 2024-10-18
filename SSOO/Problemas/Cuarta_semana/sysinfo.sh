242ddafcf0057a3e7ce9c8f207165d22fa9f1621
Joancgm05 <alu0101656571@ull.edu.es>
2024-10-18 11:11:45 +0100

    mc

--- SSOO/Problemas/Cuarta_semana/sysinfo.sh
+++ SSOO/Problemas/Cuarta_semana/sysinfo.sh
@@ -9,6 +9,7 @@
 TITLE="Información del sistema para $HOSTNAME"
 RIGHT_NOW=$(date +"%x %r%Z")
 TIME_STAMP="Actualizada el $RIGHT_NOW por $USER"
+DRIVE_SPACE=$(df -h | tr -s ' ' | cut -d ' ' -f 3,5,6)
 
 ##### Estilos
 
@@ -25,34 +26,66 @@ system_info() {
   echo "${TEXT_ULINE}Versión del sistema${TEXT_RESET}"
   echo
   uname -a  # El comando uname se utiliza para mostrar información 
-            # del sistema, la opción -a la muestra toda.
+  echo      # del sistema, la opción -a la muestra toda.
 }
 
 show_uptime() {
   echo "${TEXT_ULINE}Tiempo de encendido del sistema${TEXT_RESET}"
   echo
   uptime 
+  echo
 }
 
 drive_space() {
-  echo "Mostrando espacio ocupado por las particiones / discos duros del sitema..."
+  echo "${TEXT_GREEN}Mostrando espacio ocupado por las particiones / discos duros del sitema...${TEXT_RESET}"
   df -h | tr -s ' ' | cut -d ' ' -f 3,5,6
+  echo
 }
 
 home_space() {
   # Función de stub temporal
   echo "función home_space"
+  echo
+}
+
+is_a_file() {
+  if [ -f clase.txt ]; then
+    echo "Tienes un archivo clase.txt Todo correcto"
+    echo
+  else
+    echo "No tienes un archivo clase.txt todo mal :("
+    echo
+  fi
+}
+
+user_is_root() {
+  if [ "$USER" != root ]; then
+    echo "No tienes permisos de superusuario"
+    echo
+  else
+    echo "Tienes permisos de superusuario"
+    echo
+  fi
 }
 
 ##### Programa principal
 
 cat << _EOF_
+
 ${TEXT_RED}$TITLE${TEXT_RESET}
 
 ${TEXT_YELLOW}$TIME_STAMP${TEXT_RESET}
+
+$(system_info)
+
+$(show_uptime)
+
+$(drive_space)
+
+$(is_a_file)
+
+$(user_is_root)
+
 _EOF_
 
-system_info
-show_uptime
-drive_space
-home_space
+exit 0
